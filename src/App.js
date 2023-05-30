f9535b95f80e2c120a5e67f09e7e28532e9bff24
sagmhatr1 <sagar.mhatre@publicissapient.com>
2023-05-30 20:09:48 +0530

    files added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
+import Home from './Home.js';
+import Contact from './Contact.js';
+import About from './About';
+import NavLink from './component/NavLink';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <Link to="/" className="nav-link" activeClassName="active" aria-current="page">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/contact" className="nav-link" activeClassName="active">Contact</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/About" className="nav-link" activeClassName="active">About</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/About" className="nav-link" activeClassName="active">Products</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
